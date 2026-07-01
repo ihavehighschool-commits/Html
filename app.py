6d0a39b665154657c1ad5057f909436b399ec0fd
ihavehighschool-commits <ihavehighschool@gmail.com>
2026-07-01 19:06:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -17,13 +17,24 @@ def home():
     </html>
     """
 
+from flask import Flask, send_from_directory
+import os
+
+app = Flask(__name__)
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 @app.route("/sombath/laos")
-def sombath():
-    return render_template("index.html")
+def index():
+    return send_from_directory(BASE_DIR, "index.html")
+
+@app.route("/style.css")
+def style():
+    return send_from_directory(BASE_DIR, "style.css")
+
+@app.route("/game.js")
+def js():
+    return send_from_directory(BASE_DIR, "game.js")
 
 if __name__ == "__main__":
-    app.run(
-        host="0.0.0.0",
-        port=5000,
-        debug=True
-    )
+    app.run(host="0.0.0.0", port=5000, debug=True)
